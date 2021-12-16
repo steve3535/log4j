1f42d9be6d3f9f7fdf453750f0bafd2a955b93a2
steve3535 <stefimeister@gmail.com>
2021-12-16 16:03:14 +0100

    modif on unzip options used

--- log4j-scanner/files/check_log4j.sh
+++ log4j-scanner/files/check_log4j.sh
@@ -7,7 +7,7 @@ else
         SEARCH_DIRECTORY="$1"
 fi
 for i in `grep -r --include \*.jar log4j ${SEARCH_DIRECTORY} -l 2>/dev/null` ;do
-  unzip -l $i | grep 'log4j.*jar' >> "$FACT_FILE"
+  unzip -Z1 $i | grep 'log4j.*jar' >> "$FACT_FILE"
   if [ $? -eq 0 ];then
     echo -e "  \e[33m ==> Occurence found in $i \e[m" >> "$FACT_FILE"
   fi
