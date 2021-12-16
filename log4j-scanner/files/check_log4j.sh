9234684906aba8576d926fa89fab0727ffadf9c0
steve3535 <stefimeister@gmail.com>
2021-12-16 17:54:07 +0100

    some changes

--- log4j-scanner/files/check_log4j.sh
+++ log4j-scanner/files/check_log4j.sh
@@ -7,8 +7,14 @@ else
         SEARCH_DIRECTORY="$1"
 fi
 for i in `grep -r --include \*.jar log4j ${SEARCH_DIRECTORY} -l 2>/dev/null` ;do
-  unzip -Z1 $i | grep 'log4j.*jar' >> "$FACT_FILE"
+  buff=$(unzip -Z1 $i | grep 'log4j.*jar') 
   if [ $? -eq 0 ];then
     echo -e "  \e[33m ==> Occurence found in $i \e[m" >> "$FACT_FILE"
+    echo "$buff" >/tmp/buff
+    while read -r -a line
+    do
+      echo "   ${line[@]}" >>  "$FACT_FILE"
+    done < /tmp/buff
   fi
 done
+
